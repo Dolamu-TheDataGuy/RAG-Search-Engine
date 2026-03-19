587bc9dc7d7feb91f5245592936ca71012670567
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2026-03-19 16:26:23 +0100

    E: corrected partial search function to compare each token from query and movie title

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,13 +1,16 @@
-from lib.search_utils import DEFAULT_SEARCH_LIMIT, load_movies
 import string
+from turtle import title
+
+from lib.search_utils import DEFAULT_SEARCH_LIMIT, load_movies, partial_search
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        query_punctuation = query.translate(str.maketrans("", "", string.punctuation)) # remove punctuation from query
-        movie_no_punctuation = movie['title'].translate(str.maketrans("", "", string.punctuation)) # remove punctuation from movie title
-        if query_punctuation.lower() in movie_no_punctuation.lower():
+        query_no_punctuation = query.translate(str.maketrans("", "", string.punctuation)) # remove punctuation from query
+        title_no_punctuation = movie["title"].translate(str.maketrans("", "", string.punctuation)) # remove punctuation from movie title
+        
+        if partial_search(query_no_punctuation, title_no_punctuation):
             results.append(movie)
             if len(results) >= limit:
                 break
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import string
 
 DEFAULT_SEARCH_LIMIT = 5
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -12,3 +13,14 @@ def load_movies() -> list[dict]:
         data = json.load(f)
     return data["movies"]
         
+def partial_search(query, title):
+    split_query = query.lower().split()
+    split_query = [word for word in split_query if word] # remove empty strings from split query
+    split_title = title.lower().split()
+    split_title = [word for word in split_title if word] # remove empty strings from split title
+    
+    for word in split_query:
+        for token in split_title:
+            if word in token:
+                return True
+    return False
