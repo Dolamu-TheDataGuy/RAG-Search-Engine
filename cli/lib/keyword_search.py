57b786ffc3d1fafbac98506f51e7bc8146a22512
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2026-03-19 15:03:52 +0100

    D: Added functionality to remove punctuation from query and movie title

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,10 +1,13 @@
 from lib.search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+import string
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        if query.lower() in movie['title'].lower():
+        query_punctuation = query.translate(str.maketrans("", "", string.punctuation)) # remove punctuation from query
+        movie_no_punctuation = movie['title'].translate(str.maketrans("", "", string.punctuation)) # remove punctuation from movie title
+        if query_punctuation.lower() in movie_no_punctuation.lower():
             results.append(movie)
             if len(results) >= limit:
                 break
