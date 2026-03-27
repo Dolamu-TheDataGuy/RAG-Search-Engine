3b32ef381791c000123a71406dc2ee2411a36d50
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2026-03-27 17:22:50 +0100

    N: Restructured search_command() function and also revamp the build_command function

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -21,24 +21,33 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     Returns:
         list[dict]: A list of matching movie records.
     """
-    movies = load_movies()
     results = []
-    for movie in movies:
-        query_tokens = tokenize_text(query)
-        title_tokens = tokenize_text(movie["title"])
-        if has_matching_tokens(query_tokens, title_tokens):
-            results.append(movie)
-            if len(results) >= limit:
-                break
-    return results
 
+    idx = InvertedIndex()
+    try:
+        idx.load()
+    except FileNotFoundError:
+        print("Index file is missing")
+        return []
+    
+    query_tokens = tokenize_text(query)
+    seen_id = set()
+    if query_tokens:
+        for query_token in query_tokens:
+            doc_ids = idx.get_documents(query_token)
+
+            for id in doc_ids:
+                if id not in seen_id:
+                    seen_id.add(id)
+                    results.append(idx.docmap[id])
+                    if len(results) >= limit:
+                        return results
+    return results
 
 def build_command() -> None:
     idx = InvertedIndex()
     idx.build()
     idx.save()
-    docs = idx.get_documents("merida")
-    print(f"First document for token 'merida' = {docs[0]}")
 
 
 def has_matching_tokens(query_tokens: list[str], title_tokens: list[str]) -> bool:
@@ -83,11 +92,10 @@ def tokenize_text(text:str) -> list[str]:
 class InvertedIndex:
     def __init__(self):
         self.index = defaultdict(set) # maps each tokenized text to a set of docs_id
-        self.docmap = dict[int, dict] = {} # maps movies id to the movie object as per movie.json
+        self.docmap = {} # maps movies id to the movie object as per movie.json
         self.index_path = os.path.join(CACHE_DIR, "index.pkl")
         self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
-        
-        
+
     def __add_document(self, doc_id: int, text: str) -> None:
         for word in tokenize_text(text):
             if word not in self.index:
@@ -101,24 +109,32 @@ class InvertedIndex:
         token = tokens[0]
         doc_ids = self.index.get(token, set())
         return sorted(doc_ids)
-        
-    
+
     def build(self):
-        movies = load_movies()
-        
+        movies = load_movies() 
+
         for movie in movies:
             self.docmap[movie["id"]] = movie
-            
+
             self.__add_document(movie["id"], f"{movie['title']} {movie['description']}")
-            
-    
+
     def save(self):
         # when working with relative directory, we set file path based on current working directory which is where the script/process is run.
         # we run our script (cli/keyword_search_cli.py) from root directory, so that means "cache" is created in root directory.fffff
         os.makedirs(CACHE_DIR, exist_ok=True)
-        
+
         with open(self.index_path, "wb") as f:
             pickle.dump(self.index, f)
-            
+
         with open(self.docmap_path, "wb") as f:
             pickle.dump(self.docmap, f)
+
+    def load(self):
+        try:
+            with open(self.index_path, "rb") as f:
+                self.index = pickle.load(f)
+
+            with open(self.docmap_path, "rb") as f:
+                self.docmap = pickle.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError("File does not exist")
